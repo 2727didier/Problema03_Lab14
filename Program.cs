e1ffc9ffad2f5b3ecb5dcacbbf022ebbe4af6efe
USUARIO <USUARIO@DESKTOP-Q5ALF4J>
2023-11-22 23:43:48 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema03_Lab14
+{
+    internal class Program
+    {
+        private static readonly int totalEncuestados;
+        private static readonly object edades;
+
+        public static object seHaVacunado { get; private set; }
+
+        static void Main(string[] args, object seHaVacunado)
+        {
+            Console.WriteLine("===================================");
+            Console.WriteLine("Datos de la encuesta");
+            Console.WriteLine("===================================");
+            Console.WriteLine("ID    | Edad | Vacunado");
+            Console.WriteLine("=======================");
+
+            for (int i = 0; i < totalEncuestados; i++)
+            {
+                Console.WriteLine(value: $"{i:D4}  |  {"edades"[i],-3}  |   {((bool)seHaVacunado ? "No" : "Si")}");
+            }
+
+            Console.WriteLine("===================================");
+            Console.WriteLine("Presione una tecla para regresar ...");
+            Console.ReadKey();
+        }
+
+    }
+}
+
